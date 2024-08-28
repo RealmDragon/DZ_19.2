452d95fd848d5fa51ea22a3a03ca8df02403fe2f
RealmDragon <realmdragon2014@yandex.ru>
2024-08-28 21:06:49 +0300

    Add files via upload

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from catalog import views
+
+app_name = 'catalog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+]
+
